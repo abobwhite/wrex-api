a784eaf262aad35f1956b4f0186168e8c0cfea71
Alex White <alex.white@daugherty.com>
2020-09-08 14:28:53 -0500

    Add default categories

---
+++ src/main/groovy/com/daugherty/wrex/changelog/Changelog_003_DefaultCategories.groovy
@@ -0,0 +1,26 @@
+package com.daugherty.wrex.changelog
+
+import com.github.cloudyrock.mongock.ChangeLog
+import com.github.cloudyrock.mongock.ChangeSet
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import org.bson.Document
+
+@ChangeLog(order = '003')
+class Changelog_003_DefaultCategories {
+  @ChangeSet(order = '001', id = 'defaultCategories', author = 'ARW0214')
+  static void addDefaultCategories(MongoDatabase db) {
+    MongoCollection<Document> branchesCollection = db.getCollection('tagCategories')
+
+    branchesCollection.insertMany([
+      new Document([name: 'Application Framework']),
+      new Document([name: 'Agile Tool']),
+      new Document([name: 'Programming Language']),
+      new Document([name: 'Conference']),
+      new Document([name: 'Certification']),
+      new Document([name: 'Meetup']),
+      new Document([name: 'Hobby']),
+      new Document([name: 'IDE'])
+    ])
+  }
+}
